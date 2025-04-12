f726f25df719f5007d29a4844338b39d2b0a671e
Ricardo ANNIS <123905385+Ricdrum14@users.noreply.github.com>
2025-04-12 02:38:42 +0200

    Ajout de modifs, 12 avril 02:38

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SingleProjectComponent } from './projects/projects-list/single-project/single-project.component';
 import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { CVComponent } from './cv/cv.component';
 
 export const routes: Routes = [
-    {path:'projects/:id', component: SingleProjectComponent},
-    {path:'projects',component:ProjectsListComponent},
-    {path:'', component:HomeComponent},
+    { path: 'cv', component: CVComponent},
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path:'projects/:id', component: SingleProjectComponent},
+    { path:'projects',component:ProjectsListComponent},
+    { path:'', component:HomeComponent},
 
 ];
--- frontend/src/app/contact/contact.component.ts
+++ frontend/src/app/contact/contact.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  formData = {
+    user_name: '',
+    user_email: '',
+    message: ''
+  };
 
+  sendEmail(): void {
+    emailjs.send(
+      'service_8o5m5bn',
+      'template_l1t4qxt',
+      this.formData,
+      'C6pOONfmccbftjOnQ'
+    ).then(
+      () => {
+        alert('Message envoyé avec succès ✅');
+        this.formData = { user_name: '', user_email: '', message: '' };
+      },
+      (error) => {
+        console.error('Erreur:', error);
+        alert('Échec de l’envoi ❌');
+      }
+    );
+  }
 }
+
+
